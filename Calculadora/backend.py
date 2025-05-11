867ac3550aac441b687e4fe3b4071b75ad70004f
Pedro <annunciatomotapedro@gmail.com>
2025-05-11 16:56:19 -0300

    salvando

---
+++ Calculadora/backend.py
@@ -0,0 +1,15 @@
+n1 = input()
+n2 = input()
+
+def soma():
+    resultado = n1 + n2
+
+def sub():
+    resultado = n1 - n2
+
+def mult():
+    resultado = n1 * n2
+
+def div():
+    resultado = n1 / n2
+
